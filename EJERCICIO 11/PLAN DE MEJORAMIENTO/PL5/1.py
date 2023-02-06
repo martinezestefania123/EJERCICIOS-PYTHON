0503cc083198d9928266c34f19c2fbd764d5d657
martinezestefania123 <118738417+martinezestefania123@users.noreply.github.com>
2023-02-06 08:52:42 -0500

    Update 1.py

--- EJERCICIO 11/PLAN DE MEJORAMIENTO/PL5/1.py
+++ EJERCICIO 11/PLAN DE MEJORAMIENTO/PL5/1.py
@@ -1,5 +1,6 @@
-edad=28
+#crear un programa que permita ingresar la edad del usuario y saber si es mayor de edad y pueda acceder 
+edad=int(input("dijite su edad: ")
 if edad >= 18:
-	print('Puedes acceder, eres mayor de edad.')
+	print('Puedes acceder, eres mayor de edad.',edad)
 else:
 	print('No puedes acceder, eres menor de edad.')
