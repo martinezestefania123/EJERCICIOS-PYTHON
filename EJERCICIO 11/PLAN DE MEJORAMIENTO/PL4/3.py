7f53b02cbe2cc79e59f86517b8fd418100c7f82d
martinezestefania123 <118738417+martinezestefania123@users.noreply.github.com>
2023-02-06 08:49:53 -0500

    Update 3.py

--- EJERCICIO 11/PLAN DE MEJORAMIENTO/PL4/3.py
+++ EJERCICIO 11/PLAN DE MEJORAMIENTO/PL4/3.py
@@ -1,3 +1,4 @@
+#hacer un programa que permita agragra una clave y un valor que no se encuentren en la lista
 def electrodomesticos(dictionary,keys,value):
     if keys not in dictionary.keys():
         dictionary[keys] = value
