85a7c721a06f31331c075661c2e044f773d7d7ab
martinezestefania123 <118738417+martinezestefania123@users.noreply.github.com>
2023-02-06 08:47:20 -0500

    Update 1.py

--- EJERCICIO 11/PLAN DE MEJORAMIENTO/PL4/1.py
+++ EJERCICIO 11/PLAN DE MEJORAMIENTO/PL4/1.py
@@ -1,3 +1,4 @@
+#hacer un diccionario que den un tipo de fruta,cuantos kilos tienen y cuanto cuesta en total
 frutas = {'Plátano':1.35, 'Manzana':0.8, 'Pera':0.85, 'Naranja':0.7}
 fruta = input('¿Qué fruta quieres? ').title()
 kg = float(input('¿Cuántos kilos? '))
