5135805951e7c032e2ebd6ff831bc00e23b0659f
martinezestefania123 <118738417+martinezestefania123@users.noreply.github.com>
2023-02-06 09:06:14 -0500

    Update 3.py

--- EJERCICIO 11/PLAN DE MEJORAMIENTO/PL6/3.py
+++ EJERCICIO 11/PLAN DE MEJORAMIENTO/PL6/3.py
@@ -1,3 +1,4 @@
+#crear un programa en donde se ingrese un valor y se pueda ir sumando de en dos en dos y añadirlos a una lista
 def funcion(tam):
     lista=[g+2 for g in range (tam)]
     print(lista)
