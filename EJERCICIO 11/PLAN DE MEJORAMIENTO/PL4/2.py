b6ac056135df31bfc1370c9e8a92a3f2d32a8b05
martinezestefania123 <118738417+martinezestefania123@users.noreply.github.com>
2023-02-06 08:48:46 -0500

    Update 2.py

--- EJERCICIO 11/PLAN DE MEJORAMIENTO/PL4/2.py
+++ EJERCICIO 11/PLAN DE MEJORAMIENTO/PL4/2.py
@@ -1,3 +1,4 @@
+#hacer un programa que permita agregar al diccionario un valor y su clave
 def fruits(dictionary,keys,value):
     if keys not in dictionary.keys():
         dictionary[keys] = value
